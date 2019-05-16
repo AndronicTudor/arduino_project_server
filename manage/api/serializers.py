55b827398984bd8779d44aedbd81b12a294cd8f1
AndronicTudor <atudorel12@yahoo.com>
2019-05-16 22:31:49 +0300

    Change serializers fields name

--- manage/api/serializers.py
+++ manage/api/serializers.py
@@ -3,13 +3,13 @@ from ..models import SensorData
 
 
 class SensorDataSerializer(serializers.Serializer):
-    arduino_id = serializers.CharField(max_length=50, required=False)
-    humidity = serializers.IntegerField(required=False)
-    temperature = serializers.IntegerField(required=False)
-    light = serializers.IntegerField(required=False)
-    longitude = serializers.CharField(max_length=7, required=False)
-    latitude = serializers.CharField(max_length=7, required=False)
+    id = serializers.CharField(max_length=50, required=False)
+    hum = serializers.IntegerField(required=False)
+    temp = serializers.IntegerField(required=False)
+    lig = serializers.IntegerField(required=False)
+    lat = serializers.CharField(max_length=7, required=False)
+    lon = serializers.CharField(max_length=7, required=False)
 
     class Meta:
         model = SensorData
-        fields = '__all__'
+        fields = ('id', 'hum', 'temp', 'lig', 'lat', 'lon')
